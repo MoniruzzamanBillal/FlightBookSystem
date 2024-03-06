df428d246649fea598930f082412765487373d5d
Md Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2024-03-06 22:01:33 +0600

    setup

---
+++ flbs_client/src/Routes/Router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const Routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <p>Home </p>,
+  },
+]);
+
+export default Routes;
